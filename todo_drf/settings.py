87be0c9292cd6300f917e768e4b4fb8d839eced8
unknown <ritammoharana32@gmail.com>
2022-05-26 03:03:28 +0530

    first commit

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -129,8 +129,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'frontend/build/static')
+# STATICFILES_DIRS = [
+#      os.path.join(BASE_DIR, 'frontend/build/static')
+# ]
+
+STATIC_ROOT = [
+     os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 CORS_ORIGIN_ALLOW_ALL=True
