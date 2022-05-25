73117e24ace964a82d6b209495fa7f7ad6d22a0b
unknown <ritammoharana32@gmail.com>
2022-05-26 03:15:42 +0530

    modify settings

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# STATICFILES_DIRS = [
-#      os.path.join(BASE_DIR, 'frontend/build/static')
-# ]
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, 'frontend/build/static')
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
